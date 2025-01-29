a6a20c98bd9ad2776e4ec3739d3d5c2882c18ba6
saripalli <venusaaripalli69@gmail.com>
2025-01-29 22:00:06 +0530

    Added Railway domain to ALLOWED_HOSTS

--- portifolio/portifolio/settings.py
+++ portifolio/portifolio/settings.py
@@ -126,3 +126,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = [
+    'portifolio-production-b62d.up.railway.app',  # Your Railway domain
+    'localhost',
+    '127.0.0.1',
+]
