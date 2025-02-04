49985c8819949a9020b33def3d317bdbcc906cd5
saripalli <venusaaripalli69@gmail.com>
2025-02-04 09:45:40 +0530

    update

--- portifolio/main/views.py
+++ portifolio/main/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
+
+from .models import Project
+
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import HttpResponse
 def home(request):
     return render(request,"main/home.html")
 def about(request):
     return render(request,"main/about.html")
-def contact(request):
-    return render(request,"main/contact.html")
+
+
 def projects(request):
-    return render(request,"main/projects.html")
+    projects = Project.objects.all()
+    return render(request, "main/projects.html", {"projects": projects})
+
 def skills(request):
     return render(request,"main/skills.html")
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        
+        # Sending email
+        send_mail(
+            f"New Message from {name}",
+            f"Message: {message}\n\nFrom: {name} ({email})",
+            settings.DEFAULT_FROM_EMAIL,  
+            ['venusaaripalli69@gmail.com'],  # Email to send to
+        )
+        return HttpResponse("Message sent successfully!")
+    return render(request, 'contact.html')
